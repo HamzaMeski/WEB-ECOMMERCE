702a5e16c64f199a74b13eb1f5a8999adc08bf00
HamzaMeski <meskihamza5@gmail.com>
2025-07-10 16:41:18 +0200

    backend setup

---
+++ server/src/main/java/com/ecommerce/server/entities/User.java
@@ -0,0 +1,44 @@
+package com.ecommerce.server.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String bio;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
