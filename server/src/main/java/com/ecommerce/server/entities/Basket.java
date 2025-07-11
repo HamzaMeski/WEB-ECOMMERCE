c2e92fa411b3e46e0700305d00ed644a2cd1eb81
HamzaMeski <meskihamza5@gmail.com>
2025-07-11 17:45:13 +0200

    ecom logic setup

--- server/src/main/java/com/ecommerce/server/entities/Basket.java
+++ server/src/main/java/com/ecommerce/server/entities/Basket.java
@@ -12,7 +12,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "basket")
+@Table(name = "baskets")
 public class Basket {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
