d2969bd05f7ffd8d6253e2857ff6db050b2ae593
HamzaMeski <meskihamza5@gmail.com>
2025-07-12 12:54:45 +0100

    tailwind v4 setup

--- ui/src/components/layouts/DashboardLayout.vue
+++ ui/src/components/layouts/DashboardLayout.vue
@@ -1,5 +1,7 @@
 <template>
- dash layout
+  <div class="bg-blue-500 text-white p-4 rounded-lg">
+    <h1 class="text-2xl font-bold">Hello Dashboard v4!</h1>
+  </div>
 </template>
 
 <script setup lang="ts">
